38c3cde44561668625c81848ab523b6f00c210fb
Aleksandr Zagrivnyy <zagrivnyj.af@edu.spbstu.ru>
2024-10-19 00:01:10 +0300

    test

--- OOP/practice_7/arakelyan.daniil/src/main/java/express/CoolClass.java
+++ OOP/practice_7/arakelyan.daniil/src/main/java/express/CoolClass.java
@@ -18,7 +18,6 @@ public class CoolClass {
       Class someClass = Class.forName(className);
       Object instanse = someClass.getDeclaredConstructor().newInstance();
       Method[] methods = someClass.getDeclaredMethods();
-
       for (var method : methods) {
         if (Modifier.isPrivate(method.getModifiers()) ||
             Modifier.isProtected(method.getModifiers())) {
